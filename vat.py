87eaf1048a12ef23d6c1f848d2e1834dc1021d61
realsung <sungjun0208@naver.com>
2018-11-28 00:20:05 +0900

    update

--- vat.py
+++ vat.py
@@ -7,9 +7,6 @@
 import os
 import sys
 from optparse import OptionParser 
-if(len(sys.argv) <= 1): # 인자 값이 1이상
-	print("HELP : python vap.py -h 또는 --help")
-	exit()
 
 def getSystemInfo():
 	f = open("Analysis.txt")
@@ -22,35 +19,48 @@ def getSystemInfo():
 
 			return line
 
-parser = OptionParser('''python vap.py -f [FILE]''')
-parser.add_option("-f","--file",type="string",dest="FILE",help="write report to FILE")
-(option,args) = parser.parse_args()
-
-file = option.FILE
-
-if option == None:
-	print(parser.usage)
-	exit()
-
-if option != None:
-
-	# imageinfo
-	os.system("echo 1.imageinfo ================================== > Analysis.txt")
-	os.system("vol.py -f " + file + " imageinfo >> Analysis.txt")
-	profile = "--profile="
-	profile += getSystemInfo()
-	os.system("echo >> Analysis.txt")
-	os.system("echo >> Analysis.txt")
-
-	# psxview
-	os.system("echo 2.psxview ================================== >> Analysis.txt")
-	os.system("vol.py -f " + file + " psxview " + profile + " >> Analysis.txt")
-	os.system("echo >> Analysis.txt")
-	os.system("echo >> Analysis.txt")
-
-	# pstree
-	os.system("echo 3.pstree ===================================== >> Analysis.txt")
-	os.system("vol.py -f " + file + " pstree " + profile + " >> Analysis.txt")
-	os.system("echo >> Analysis.txt")
-	os.system("echo >> Analysis.txt")
+def memoryAnalysis():
+	parser = OptionParser('''python vap.py -f [FILE]''')
+	parser.add_option("-f","--file",type="string",dest="FILE",help="write report to FILE")
+	(option,args) = parser.parse_args()
 	
+	file = option.FILE
+
+	if option == None:
+		print(parser.usage)
+		exit()
+
+	if option != None:
+		# imageinfo
+		os.system("echo 1.imageinfo >> Analysis.txt")
+		os.system("vol.py -f " + file + " imageinfo >> Analysis.txt")
+		profile = "--profile="
+		profile += getSystemInfo()
+		os.system("echo >> Analysis.txt")
+		os.system("echo >> Analysis.txt")
+
+		# psxview
+		os.system("echo 2.psxview >> Analysis.txt")
+		os.system("vol.py -f " + file + " psxview " + profile + " >> Analysis.txt")
+		os.system("echo >> Analysis.txt")
+		os.system("echo >> Analysis.txt")
+
+		# pstree
+		os.system("echo 3.pstree >> Analysis.txt")
+		os.system("vol.py -f " + file + " pstree " + profile + " >> Analysis.txt")
+		os.system("echo >> Analysis.txt")
+		os.system("echo >> Analysis.txt")
+	
+		#netscan
+		os.system("echo 4.netscan >> Analysis.txt")
+		os.system("vol.py -f " + file + " netscan " + profile + " >> Analysis.txt")
+		os.system("echo >> Analysis.txt")
+		os.system("echo >> Analysis.txt")
+
+
+if __name__ == '__main__':
+	if(len(sys.argv) <= 1): # 인자 값이 1이상
+		print("HELP : python vap.py -h 또는 --help")
+		exit()
+	else:
+		memoryAnalysis()
