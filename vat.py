8643b538195c77da8156d550ed6407786bce8855
realsung <sungjun0208@naver.com>
2018-12-16 23:00:42 +0900

    add mal

--- vat.py
+++ vat.py
@@ -6,7 +6,23 @@
 
 import os
 import sys
+import urllib
 from optparse import OptionParser 
+
+parser = OptionParser('''python vat.py -f [FILE]\npython vat.py -f [FILE] -o [Object]''')
+parser.add_option("-f","--file",type="string",dest="FILE",help="write report to FILE")
+parser.add_option("-o","--object",type="string",dest="object",help="write Address")
+(option,args) = parser.parse_args()
+	
+file = option.FILE
+
+if(len(sys.argv) <= 1): # 인자 값이 1이상
+	print("HELP : python vap.py -h 또는 --help")
+	exit()
+
+if option == None:
+	print(parser.usage)
+	exit(0)
 
 def getSystemInfo():
 	f = open("Analysis.txt")
@@ -19,55 +35,86 @@ def getSystemInfo():
 
 			return line
 
-def memoryAnalysis():
-	parser = OptionParser('''python vap.py -f [FILE]''')
-	parser.add_option("-f","--file",type="string",dest="FILE",help="write report to FILE")
-	(option,args) = parser.parse_args()
-	
-	file = option.FILE
+def getMalInfo(procName):
+    f = open("default_scan.txt")
+    list1 = []
+    for line in f.readlines():
+        if(line.find("Process")!=-1):
+            if(line.find("explorer.exe")!=-1):
+                line = line.split(":")
+                pid = line[2].split(" ")[1]
+                addr = line[3].strip()
+                list1.append([pbid, addr])
+    return list1
 
-	if option == None:
-		print(parser.usage)
-		exit()
+def getProcInfo(obj):
+    f = open("Analysis.txt.txt")
+    for line in f.readlines():
+        if(line.find(obj)!=-1):
+            line = line.split(" ")[1]
+            line = line.strip()
+            print "[*] Process Name of Obejct : " + line
+            return line
 
-	if option != None:
-		# imageinfo
-		os.system("echo 1.imageinfo >> Analysis.txt")
-		os.system("vol.py -f " + file + " imageinfo >> Analysis.txt")
-		profile = "--profile="
-		profile += getSystemInfo()
-		os.system("echo >> Analysis.txt")
-		os.system("echo >> Analysis.txt")
 
-		# psxview
-		os.system("echo 2.psxview >> Analysis.txt")
-		os.system("vol.py -f " + file + " psxview " + profile + " >> Analysis.txt")
-		os.system("echo >> Analysis.txt")
-		os.system("echo >> Analysis.txt")
+if option.object == None:
+	# imageinfo
+	os.system("echo 1.imageinfo >> Analysis.txt")
+	os.system("vol.py -f " + file + " imageinfo >> Analysis.txt")
+	profile = "--profile="
+	profile += getSystemInfo()
+	os.system("echo >> Analysis.txt")
+	os.system("echo >> Analysis.txt")
 
-		# pstree
-		os.system("echo 3.pstree >> Analysis.txt")
-		os.system("vol.py -f " + file + " pstree " + profile + " >> Analysis.txt")
-		os.system("echo >> Analysis.txt")
-		os.system("echo >> Analysis.txt")
-	
-		#netscan
-		os.system("echo 4.netscan >> Analysis.txt")
-		os.system("vol.py -f " + file + " netscan " + profile + " >> Analysis.txt")
-		os.system("echo >> Analysis.txt")
-		os.system("echo >> Analysis.txt")
+	# psxview
+	os.system("echo 2.psxview >> Analysis.txt")
+	os.system("vol.py -f " + file + " psxview " + profile + " >> Analysis.txt")
+	os.system("echo >> Analysis.txt")
+	os.system("echo >> Analysis.txt")
 
-		#filescan
-		os.system("echo 1.filescan >> filescan.txt")
-		os.system("vol.py -f " + file + "filescan" + profile + " >> filescan.txt")
-		os.system("echo >> filescan.txt")
-		os.system("echo >> filescan.txt")
+	# pstree
+	os.system("echo 3.pstree >> Analysis.txt")
+	os.system("vol.py -f " + file + " pstree " + profile + " >> Analysis.txt")
+	os.system("echo >> Analysis.txt")
+	os.system("echo >> Analysis.txt")
 
-def malAnalysis():
-	
-if __name__ == '__main__':
-	if(len(sys.argv) <= 1): # 인자 값이 1이상
-		print("HELP : python vap.py -h 또는 --help")
-		exit()
-	else:
-		memoryAnalysis()
+	#netscan
+	os.system("echo 4.netscan >> Analysis.txt")
+	os.system("vol.py -f " + file + " netscan " + profile + " >> Analysis.txt")
+	os.system("echo >> Analysis.txt")
+	os.system("echo >> Analysis.txt")
+else:
+	obj = option.object
+	profile = "--profile="
+	profile += getSystemInfo
+	procName = getProcInfo(obj)
+	malfindInfo = getMalInfo(procName)
+
+	filename = obj + ".txt"
+
+	os.system("echo object = " + obj + " > " + filename)
+	os.system("echo 1.mftparser >> " + filename)
+	os.system("vol.py -f " + file + " mftparser "+ profile + " | grep -i " + procName[:5] + " >> " + filename)
+	os.system("echo >> " + filename)
+	os.system("echo >> " + filename)
+
+	os.system("echo 2.dlllist >> " + filename)
+	os.system("vol.py -f " + file + " dlllist -o " + obj + " " + profile + " >> " + filename)
+	os.system("echo >> " + filename)
+	os.system("echo >> " + filename)
+
+	os.system("echo 3.dlldump with malfind >> " + filename)
+	if (malfindInfo != ()):
+	     for list1 in malfindInfo:
+	        os.system("vol.py -f " + vmem + " dlldump -p " + list1[0] + " -b  " + list1[1] + " " + info + " --dump-dir=./ >> " + filename )
+
+		os.system("vol.py -f " + vmem + " procdump -D ./ -o " + obj + " " + info + " >> " + filename)
+	os.system("echo >> " + filename)
+	os.system("echo >> " + filename)
+
+	os.system("echo 3.strings  >> " + filename)
+	os.system("strings *.exe >> " + filename)
+	os.system("echo >> " + filename)
+	os.system("echo >> " + filename)
+
+	urllib.urlopen("http://www.virustotal.com")
